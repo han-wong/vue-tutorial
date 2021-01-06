7953cd92c57eba60e7ef39824d081eb59cda1b06
Han Wong <han.wong@pm.me>
2021-01-06 22:21:10 +0100

    7. Computed Properties

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 var app = new Vue({
     el: '#app',
+    computed: {
+        image: function () {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock: function () {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale: function () {
+            if(this.onSale){
+                return `${this.brand} ${this.product} are on sale!`;
+            }
+            return `${this.brand} ${this.product} are not on sale!`;
+        },
+        title: function () {
+            return this.brand + ' ' + this.product;
+        }
+    },
     data: {
         altText: 'A pair of socks',
+        brand: 'Vue Mastery',
         cart: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        image: './assets/vmSocks-green-onWhite.jpg',
-        inStock: true,
-        inventory: 100,
         link: 'http://vuejs.org',
         onSale: true,
         product: 'Socks',
+        selectedVariant: 0,
         sizes: ['small', 'medium', 'large'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: './assets/vmSocks-green-onWhite.jpg'
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue-onWhite.jpg'
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ]
     },
@@ -29,10 +47,10 @@ var app = new Vue({
             this.cart += 1;
         },
         removeFromCart() {
-            if (this.cart) this.cart -= 1;
+            this.cart -= 1;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage;
+        updateProduct(index) {
+            this.selectedVariant = index;
         }
     }
 })
