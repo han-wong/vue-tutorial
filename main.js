f8d31014fecefd09ae95d8d8df28eeda7cfd05b4
Han Wong <han.wong@pm.me>
2021-01-07 10:14:05 +0100

    10. Forms & v-model

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ Vue.component('product', {
             link: 'http://vuejs.org',
             onSale: true,
             product: 'Socks',
+            reviews: [],
             selectedVariant: 0,
             sizes: ['small', 'medium', 'large'],
             variants: [
@@ -50,6 +51,9 @@ Vue.component('product', {
         };
     },
     methods: {
+        addReview(productReview) {
+            this.reviews.push(productReview);
+        },
         addToCart() {
             this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
@@ -94,6 +98,19 @@ Vue.component('product', {
                 <button @click="addToCart" :class="{ disabledButton: !inStock }" :disabled="!inStock">Add to cart</button>
                 <button @click="removeFromCart" :class="{ disabledButton: !cart.includes(variants[selectedVariant].variantId) }" :disabled="!cart">Remove from cart</button>
             </div>
+            <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length"> There are no reviews yet. </p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p> {{ review.name }} </p>
+                        <p> Rating: {{ review.rating }} </p>
+                        <p> {{ review.review }} </p>
+                        <p> Would you recommend this product?: {{ review.recommend }} </p>
+                    </li>
+                </ul>
+            </div>
+            <product-review @review-submitted="addReview"></product-review>
         </div>
     `
 });
@@ -109,6 +126,79 @@ Vue.component('product-details', {
         <ul>
             <li v-for="detail in details"> {{ detail }} </li>
         </ul>
+    `
+});
+
+Vue.component('product-review', {
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        };
+    },
+    methods: {
+        onSubmit() {
+            this.errors = [];
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                };
+                this.$emit('review-submitted', productReview);
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+                this.recommend = null;
+            }
+            else {
+                if (!this.name) this.errors.push('Name required.');
+                if (!this.review) this.errors.push('Review required.');
+                if (!this.rating) this.errors.push('Rating required.');
+                if (!this.recommend) this.errors.push('Recommendation required.');
+            }
+        }
+    },
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+            <p v-if="errors.length">
+                <b> Please correct the following error(s): </b>
+                <ul>
+                    <li v-for="error in errors"> {{ error }} </li>
+                </ul>
+            </p>
+            <p>
+                <label for="name">Name:</label>
+                <input type="text" id="name" v-model="name" placeholder="Name"></input>
+            </p>
+            <p>
+                <label for="review">Review:</label>
+                <textarea name="" id="review" cols="30" rows="10" v-model="review"></textarea>
+            </p>
+            <p>
+                <label for="rating">Rating:</label>
+                <select name="" id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+            <p> Would you recommend this product? <br>
+                <label>Yes:</label>
+                <input type="radio" v-model="recommend" value="Yes" checked />
+                <label>No:</label>
+                <input type="radio" v-model="recommend" value="No">
+            </p>
+            <p>
+                <input type="submit" value="submit">
+            </p>
+        </form>
     `
 });
 
